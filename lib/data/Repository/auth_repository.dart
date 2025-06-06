6166be0ae6aea100139fe322a3e413b670106e11
iamanggi <anggipuspita452@gmail.com>
2025-06-06 21:17:54 +0700

    Add login and register methods

--- lib/data/Repository/auth_repository.dart
+++ lib/data/Repository/auth_repository.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
+import 'package:canary/data/model/request/auth/login_request_model.dart';
+import 'package:canary/data/model/request/auth/register_request_model.dart';
+import 'package:canary/data/model/response/auth/auth_response_model.dart';
+import 'package:canary/data/model/response/auth/login_response_model.dart';
 import 'package:canary/services/service_http_client.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthRepository {
@@ -6,5 +13,52 @@ class AuthRepository {
   final secureStorage = FlutterSecureStorage();
 
   AuthRepository(this._serviceHttpClient);
-  
+
+  Future<Either<String, AuthResponseModel>> login(
+    LoginRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        'login',
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final loginResponse = AuthResponseModel.fromMap(jsonResponse);
+        await secureStorage.write(
+          key: "authToken",
+          value: loginResponse.user!.token,
+        );
+        await secureStorage.write(
+          key: "userRole",
+          value: loginResponse.user!.role,
+        );
+        return Right(loginResponse);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Login failed');
+      }
+    } catch (e) {
+      return Left("An error occured while logging in:");
+    }
+  }
+
+  Future<Either<String, String>> register(
+    RegisterRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "register",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      final registerResponse = jsonResponse['message'];
+      if (response.statusCode == 201) {
+        return Right(registerResponse);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Registration failed');
+      }
+    } catch (e) {
+      return Left("An error occured while registering. : $e");
+    }
+  }
 }
